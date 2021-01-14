38393cfa985617d819a5fe83abe8deeb1f5f28c7
MikeP <matteodalessandro.dr@gmail.com>
2021-01-14 13:53:40 +0100

    adding Genres enum to model package

---
+++ src/main/java/it/soundmate/model/Genres.java
@@ -0,0 +1,17 @@
+package it.soundmate.model;
+
+import java.util.Arrays;
+
+public enum Genres {
+
+    ROCK, POP, BLUES, JAZZ, METAL, HIPHOP, ELECTRONIC, INDIE, FOLK, FUNKY, FUSION;
+
+    public static Genres returnGenre(String genre){
+        Genres[] genres = Genres.values();
+        return Arrays.stream(genres)
+                .filter(currentGenre -> currentGenre.toString().equals(genre))
+                .findFirst().get();
+    }
+
+
+}
