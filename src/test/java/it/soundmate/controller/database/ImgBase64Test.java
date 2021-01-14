87059ee4ab02f1e7b3b8a93f65f8e54fe8df2544
MikeP <matteodalessandro.dr@gmail.com>
2021-01-14 12:47:51 +0100

    adding tests

---
+++ src/test/java/it/soundmate/controller/database/ImgBase64Test.java
@@ -0,0 +1,56 @@
+package it.soundmate.controller.database;
+
+import it.soundmate.utils.ImgBase64Repo;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Base64;
+
+public class ImgBase64Test {
+
+
+    @TempDir
+    public File folder;
+    private Path imgPath;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        imgPath = Paths.get(
+                ImgBase64Test.class.getClassLoader()
+                        .getResource("Gpus.jpg")
+                        .toURI()
+        );
+    }
+
+    @Test
+    public void encode() throws IOException {
+
+        File created = new File(folder, "gpus");
+        String encodedImage = ImgBase64Repo.encode(imgPath);
+
+        byte[] decodedBytes = Base64.getDecoder().decode(encodedImage);
+        Files.write(created.toPath(), decodedBytes);
+
+        Assertions.assertThat(created).hasSameBinaryContentAs(imgPath.toFile());
+    }
+
+    @Test
+    public void decode() throws IOException {
+        File toWrite = new File(folder, "testImg");
+        String encodedImage = ImgBase64Repo.encode(imgPath);
+
+        ImgBase64Repo.decode(encodedImage, toWrite.toPath());
+
+        Assertions.assertThat(toWrite).hasSameBinaryContentAs(imgPath.toFile());
+    }
+
+
+
+}
