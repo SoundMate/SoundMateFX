51f19fb8e8c01c81419a2ad97c3d6cb462234134
MikeP <matteodalessandro.dr@gmail.com>
2021-01-22 00:47:19 +0100

    DBScript refactor

--- src/main/resources/DBscript.sql
+++ src/main/resources/DBscript.sql
@@ -1,3 +1,14 @@
+create table banned_users
+(
+    email text not null
+);
+
+alter table banned_users
+    owner to postgres;
+
+create unique index banned_users_email_uindex
+    on banned_users (email);
+
 create table registered_users
 (
     id        serial not null
@@ -5,7 +16,8 @@ create table registered_users
             primary key,
     email     text   not null,
     password  text   not null,
-    user_type text   not null
+    user_type text   not null,
+    city      varchar
 );
 
 alter table registered_users
@@ -30,28 +42,6 @@ create table users
 
 alter table users
     owner to postgres;
-
-create unique index users_id_uindex
-    on users (id);
-
-create table solo
-(
-    id         integer not null
-        constraint solo_pk
-            primary key
-        constraint solo_users_id_fk
-            references users
-            on update cascade on delete cascade,
-    age        integer,
-    first_name text,
-    last_name  text
-);
-
-alter table solo
-    owner to postgres;
-
-create unique index solo_id_uindex
-    on solo (id);
 
 create table band
 (
@@ -85,19 +75,57 @@ create table band_manager
 alter table band_manager
     owner to postgres;
 
+create table band_band_managed
+(
+    id_manager integer not null
+        constraint band_band_managed_band_manager_id_fk
+            references band_manager
+            on update cascade on delete cascade,
+    id_band    integer not null
+        constraint band_band_managed_band_id_fk
+            references band
+            on update cascade on delete cascade
+);
+
+alter table band_band_managed
+    owner to postgres;
+
+create unique index band_band_managed_id_band_uindex
+    on band_band_managed (id_band);
+
+create unique index band_band_managed_id_manager_uindex
+    on band_band_managed (id_manager);
+
 create unique index band_manager_id_uindex
     on band_manager (id);
 
-create table room_manager
+create table played_genres
 (
-    id         integer not null
+    id    integer not null
+        constraint played_genres_pk
+            primary key
+        constraint played_genres_band_id_fk
+            references band
+            on update cascade on delete cascade,
+    genre text    not null
+);
+
+alter table played_genres
+    owner to postgres;
+
+create unique index played_genres_id_uindex
+    on played_genres (id);
+
+create table room_renter
+(
+    id      integer not null
         constraint room_manager_pk
             primary key
         constraint room_manager_users_id_fk
             references users
             on update cascade on delete cascade,
-    first_name text    not null,
-    last_name  text    not null
+    name    varchar not null,
+    address varchar not null
 );
 
 alter table room_renter
@@ -112,16 +140,15 @@ create table room
     room_code    serial               not null
         constraint room_pk
             primary key,
-    room_address text                 not null,
     room_price   numeric              not null,
-    room_is_free boolean default true not null
+    room_is_free boolean default true not null,
+    photo        text,
+    description  text,
+    name         varchar              not null
 );
 
 alter table room
     owner to postgres;
-
-create unique index room_id_uindex
-    on room (id);
 
 create unique index room_room_code_uindex
     on room (room_code);
@@ -129,16 +156,21 @@ create unique index room_room_code_uindex
 create unique index room_manager_id_uindex
     on room_renter (id);
 
-create table banned_users
+create table solo
 (
-    email text not null
+    id         integer not null
+        constraint solo_pk
+            primary key
+        constraint solo_users_id_fk
+            references users
+            on update cascade on delete cascade,
+    age        integer,
+    first_name text,
+    last_name  text
 );
 
-alter table banned_users
+alter table solo
     owner to postgres;
-
-create unique index banned_users_email_uindex
-    on banned_users (email);
 
 create table band_solo_members
 (
@@ -161,26 +193,22 @@ create unique index band_solo_members_id_band_uindex
 create unique index band_solo_members_id_solo_uindex
     on band_solo_members (id_solo);
 
-create table band_band_managed
+create table fav_genres
 (
-    id_manager integer not null
-        constraint band_band_managed_band_manager_id_fk
-            references band_manager
+    id    integer not null
+        constraint fav_genres_pk
+            primary key
+        constraint fav_genres_solo_id_fk
+            references solo
             on update cascade on delete cascade,
-    id_band    integer not null
-        constraint band_band_managed_band_id_fk
-            references band
-            on update cascade on delete cascade
+    genre text[]  not null
 );
 
-alter table band_band_managed
+alter table fav_genres
     owner to postgres;
 
-create unique index band_band_managed_id_band_uindex
-    on band_band_managed (id_band);
-
-create unique index band_band_managed_id_manager_uindex
-    on band_band_managed (id_manager);
+create unique index fav_genres_id_uindex
+    on fav_genres (id);
 
 create table played_instruments
 (
@@ -199,37 +227,9 @@ alter table played_instruments
 create unique index instruments_id_uindex
     on played_instruments (id);
 
-create table fav_genres
-(
-    id    integer not null
-        constraint fav_genres_pk
-            primary key
-        constraint fav_genres_solo_id_fk
-            references solo
-            on update cascade on delete cascade,
-    genre text[]  not null
-);
+create unique index solo_id_uindex
+    on solo (id);
 
-alter table fav_genres
-    owner to postgres;
-
-create unique index fav_genres_id_uindex
-    on fav_genres (id);
-
-create table played_genres
-(
-    id    integer not null
-        constraint played_genres_pk
-            primary key
-        constraint played_genres_band_id_fk
-            references band
-            on update cascade on delete cascade,
-    genre text    not null
-);
-
-alter table played_genres
-    owner to postgres;
-
-create unique index played_genres_id_uindex
-    on played_genres (id);
+create unique index users_id_uindex
+    on users (id);
 
