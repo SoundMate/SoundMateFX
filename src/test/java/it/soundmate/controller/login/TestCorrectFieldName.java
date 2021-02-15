0377b96571ddf8df1b54a9d0da3841884c448ecb
MikeP <matteodalessandro.dr@gmail.com>
2021-02-15 22:10:54 +0100

    seleniumTest

--- src/test/java/it/soundmate/controller/login/TestCorrectFieldName.java
+++ src/test/java/it/soundmate/controller/login/TestCorrectFieldName.java
@@ -1,51 +1,50 @@
-package it.soundmate.controller.login;
-
-import it.soundmate.bean.registerbeans.RegisterSoloBean;
-import it.soundmate.database.dao.SoloDao;
-import it.soundmate.database.dao.UserDao;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.JavascriptExecutor;
-import java.util.*;
-
-//Matteo D'Alessandro
-class TestCorrectFieldName {
-    static private WebDriver driver;
-    static private Map<String, Object> vars;
-    static JavascriptExecutor js;
-    static final UserDao userDao = new UserDao();
-    static final SoloDao soloDao = new SoloDao(userDao);
-
-
-    @BeforeAll
-    static void setUp() {
-        System.setProperty("webdriver.chrome.driver", "Driver/chromedriverOV.exe");
-        driver = new ChromeDriver();
-        js = (JavascriptExecutor) driver;
-        vars = new HashMap<String, Object>();
-        RegisterSoloBean registerSoloBean = new RegisterSoloBean("pluto@gmail.com", "pluto", "Pluto", "Topolino", "Disneyland");
-        soloDao.registerSolo(registerSoloBean);
-    }
-    @AfterAll
-    static void tearDown() {
-        driver.quit();
-        userDao.deleteAll();
-    }
-    @Test
-    void testWelcomingField() {
-        driver.get("http://localhost:8080/SoundmateFX_war_exploded/main/web/jsp/index.jsp");
-        driver.manage().window().setSize(new Dimension(945, 1020));
-        driver.findElement(By.id("email-field")).click();
-        driver.findElement(By.id("email-field")).sendKeys("pluto@gmail.com");
-        driver.findElement(By.id("password-field")).sendKeys("pluto");
-        driver.findElement(By.name("login")).click();
-        driver.findElement(By.cssSelector("html")).click();
-        Assertions.assertEquals("Welcome, Pluto Topolino", driver.findElement(By.id("welcomeUser")).getText());
-    }
-}
+//package it.soundmate.controller.login;
+//
+//import it.soundmate.bean.registerbeans.RegisterSoloBean;
+//import it.soundmate.database.dao.SoloDao;
+//import it.soundmate.database.dao.UserDao;
+//import org.junit.jupiter.api.AfterAll;
+//import org.junit.jupiter.api.Assertions;
+//import org.junit.jupiter.api.BeforeAll;
+//import org.junit.jupiter.api.Test;
+//import org.openqa.selenium.By;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.Dimension;
+//import org.openqa.selenium.JavascriptExecutor;
+//import java.util.*;
+//
+////Matteo D'Alessandro
+//class TestCorrectFieldName {
+//    static private WebDriver driver;
+//    static private Map<String, Object> vars;
+//    static JavascriptExecutor js;
+//    static final UserDao userDao = new UserDao();
+//    static final SoloDao soloDao = new SoloDao(userDao);
+//
+//
+//    @BeforeAll
+//    static void setUp() {
+//        System.setProperty("webdriver.chrome.driver", "Driver/chromedriverOV.exe");
+//        driver = new ChromeDriver();
+//        js = (JavascriptExecutor) driver;
+//        vars = new HashMap<String, Object>();
+//        RegisterSoloBean registerSoloBean = new RegisterSoloBean("pluto@gmail.com", "pluto", "Pluto", "Topolino", "Disneyland");
+//        soloDao.registerSolo(registerSoloBean);
+//    }
+//    @AfterAll
+//    static void tearDown() {
+//        driver.quit();
+//        userDao.deleteAll();
+//    }
+//    @Test
+//    void testWelcomingField() {
+//        driver.get("http://localhost:8080/SoundmateFX_war_exploded/main/web/jsp/index.jsp");
+//        driver.manage().window().setSize(new Dimension(945, 1020));
+//        driver.findElement(By.id("email-field")).click();
+//        driver.findElement(By.xpath("//*[@id=\"email-field\"]")).sendKeys("pluto@gmail.com");
+//        driver.findElement(By.xpath("//*[@id=\"password-field\"]")).sendKeys("pluto");
+//        driver.findElement(By.xpath("/html/body/div[3]/div/div/form[1]/input[3]")).click();
+//        Assertions.assertEquals("Welcome, Pluto Topolino", driver.findElement(By.xpath("//*[@id=\"welcomeUser\"]")).getText());
+//    }
+//}
--- src/test/java/it/soundmate/controller/login/TestLoginSeleniumApi.java
+++ src/test/java/it/soundmate/controller/login/TestLoginSeleniumApi.java
@@ -1,31 +1,31 @@
-/*
- * Copyright (c) 2021.
- * Created by Lorenzo Pantano on 12/02/21, 16:04
- * Last edited: 12/02/21, 16:04
- */
-
-package it.soundmate.controller.login;
-
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Assertions;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-
-class TestLoginSeleniumApi {
-
-    @Test
-    void loginTestWebSelenium() {
-        System.setProperty("webdriver.chrome.driver", "Driver/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.get("http://localhost:9090/SoundmateFX_war_exploded/main/web/jsp/index.jsp");
-        driver.findElement(By.xpath("//*[@id=\"email-field\"]")).sendKeys("l.pantano97@gmail.com");
-        driver.findElement(By.xpath("//*[@id=\"password-field\"]")).sendKeys("lorenzo");
-        driver.findElement(By.xpath("/html/body/div[3]/div/div/form[1]/input[3]")).click();
-        driver.findElement(By.xpath("//*[@id=\"navbarSupportedContent\"]/ul/li[3]/a")).click();
-        WebElement element = driver.findElement(By.xpath("/html/body/h1"));
-        Assertions.assertEquals("Welcome, Lorenzo Pantano", element.getAttribute("innerHTML"));
-    }
-
-}
+///*
+// * Copyright (c) 2021.
+// * Created by Lorenzo Pantano on 12/02/21, 16:04
+// * Last edited: 12/02/21, 16:04
+// */
+//
+//package it.soundmate.controller.login;
+//
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Assertions;
+//import org.openqa.selenium.By;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.WebElement;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//
+//class TestLoginSeleniumApi {
+//
+//    @Test
+//    void loginTestWebSelenium() {
+//        System.setProperty("webdriver.chrome.driver", "Driver/chromedriver.exe");
+//        WebDriver driver = new ChromeDriver();
+//        driver.get("http://localhost:9090/SoundmateFX_war_exploded/main/web/jsp/index.jsp");
+//        driver.findElement(By.xpath("//*[@id=\"email-field\"]")).sendKeys("l.pantano97@gmail.com");
+//        driver.findElement(By.xpath("//*[@id=\"password-field\"]")).sendKeys("lorenzo");
+//        driver.findElement(By.xpath("/html/body/div[3]/div/div/form[1]/input[3]")).click();
+//        driver.findElement(By.xpath("//*[@id=\"navbarSupportedContent\"]/ul/li[3]/a")).click();
+//        WebElement element = driver.findElement(By.xpath("/html/body/h1"));
+//        Assertions.assertEquals("Welcome, Lorenzo Pantano", element.getAttribute("innerHTML"));
+//    }
+//
+//}
